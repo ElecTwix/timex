1d3716a0be6be94c0b9cfa53d56a243657e8a255
ElecTwix <51170038+ElecTwix@users.noreply.github.com>
2023-06-18 14:27:33 +0300

    refactor

--- main.go
+++ main.go
@@ -2,15 +2,31 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
 	"time"
 )
 
-var clear *exec.Cmd = exec.Command("tput cuu 1")
+// Errors
+var (
+	errNoBin = errors.New("there is no bin found")
+	errUsage = errors.New("usage timex <command/bin> <args>")
+)
 
 func main() {
+	err := Mesure()
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+	}
+}
+
+func Mesure() error {
+	if len(os.Args) < 2 {
+		fmt.Println(errUsage)
+		return errNoBin
+	}
 	binName := os.Args[1]
 	args := os.Args[2:]
 
@@ -18,27 +34,24 @@ func main() {
 	if err != nil {
 		_, err = os.Stat(binName)
 		if err != nil {
-			panic("file not exists")
+			return err
 		}
 		path = "./" + binName
 	}
 
 	ctx := context.Background()
-
 	cmd := exec.CommandContext(ctx, path, args...)
-
 	startTime := time.Now()
-
 	go TimeCount(startTime)
 
 	err = cmd.Run()
-
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	fmt.Printf("\033[1A\033[K")
 	fmt.Printf("time: %s\n", time.Since(startTime))
+	return nil
 }
 
 func TimeCount(counter time.Time) {
---
+++ readme.md
@@ -0,0 +1,12 @@
+# TIMEX
+------
+## Install
+```bash
+go install github.com/ElecTwix/timex@latest
+```
+
+## Usage
+```bash
+timex <command/binary> <args>
+```
+
